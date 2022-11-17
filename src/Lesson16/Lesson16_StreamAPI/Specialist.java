fddb36946b6e37a415ff5d449131fa21cacfb83d
domambo77 <domambo77@mail.ru>
2022-11-17 19:51:35 +0300

    еще одна попытка выложить файлы на git

---
+++ src/Lesson16/Lesson16_StreamAPI/Specialist.java
@@ -0,0 +1,45 @@
+package Lesson16.Lesson16_StreamAPI;
+
+
+public class Specialist {
+    //Поля
+    private String name;
+    private Integer salary;
+    private Specialty specialty;
+
+    //Конструктор
+
+    public Specialist(String name, Integer salary, Specialty specialty) {
+        this.name = name;
+        this.salary = salary;
+        this.specialty = specialty;
+
+    }
+
+
+    //Методы
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Integer salary) {
+        this.salary = salary;
+    }
+
+    public Specialty getSpecialty() {
+        return specialty;
+    }
+
+    public void setSpecialty(Specialty specialty) {
+        this.specialty = specialty;
+    }
+}
